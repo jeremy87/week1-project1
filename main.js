bffd23a8ef29bde36e83584c46031a3c6468021d
jeremy87 <jeremy.pike87@gmail.com>
2016-01-20 12:26:34 -0700

    Week 1 Project 1 Answers

--- main.js
+++ main.js
@@ -5,18 +5,38 @@
 
 //1. Log your name to the console (as a string).
 
+	console.log("Jeremy")
+
 //2. Declare a variable called name, log it to the console.
+	
+	var x = 'name';
+	console.log(x)
 
 //3. Declare two variables with number values. Add, subtract, multiply and divide them.
 
+	var y = 12;
+	var z = 3;
+
+	console.log(y + z)
+	console.log(y - z)
+	console.log(y / z)
+	console.log(y * z)
+
 //4. Declare two variables with string values. Create a third variable that concatenates them.
+
+	var work = 'Broadnet';
+	var location = 'Highlands Ranch';
+	var answer = 'I work at ' + work + ' located in ' + location '.';
+	document.getElementByID("question4").innerHTML = answer;
 
 //5. Add, subract, multiply and divide the strings '5' and '3'. Observe and explain the results.
 
 //6. Create a prompt that asks a user to pick a number. Double the answer.
 
 //7. Create a custom alert.
-
+	
+	window.alert("I am an alert")
+ 
 //8. Declare three variables called length, width and height. Calculate area and volume.
 
 //9. Ask for a user's name, age and city. In a single command display/log the sentence:
