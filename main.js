74063c6291c647facbc91891fdf603896d10bd41
jeremy87 <jeremy.pike87@gmail.com>
2016-01-20 20:01:35 -0700

    Completed homework

--- main.js
+++ main.js
@@ -31,7 +31,26 @@
 
 //5. Add, subract, multiply and divide the strings '5' and '3'. Observe and explain the results.
 
+	"5" + "3"
+	53
+
+	// Adding string together concatenates the numbers
+
+	"5" - "3"
+	2
+
+	"5" * "3"
+	15
+
+	"5" / "3"
+	1.6666666666666667
+
+	// All other math operations work as expected with numerals in a string
+
 //6. Create a prompt that asks a user to pick a number. Double the answer.
+
+	var number = prompt("Please pick a number.")
+	console.log(number * 2)
 
 //7. Create a custom alert.
 	
@@ -39,7 +58,23 @@
  
 //8. Declare three variables called length, width and height. Calculate area and volume.
 
+	var length = '34';
+	var width = '12';
+	var height = '5';
+
+	console.log(length * width * height)
+	2040
+
 //9. Ask for a user's name, age and city. In a single command display/log the sentence:
 // "My name is...I'm....years old, and I live in....."
 
+	var name = prompt("What is your name?")
+	var age = prompt("How old are you?")
+	var city = prompt("Where do you live?")
+
+	confirm('My name is ' + name + '.' + ' I am ' + age + ' years old, and I live in ' + city)
+
 //10. Create an alert that uses the response from a prompt.
+
+	var response = prompt('What is your favorite animal?')
+	alert('Your favorite animal is a ' + response + '.')
